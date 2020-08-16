e3be62a8124d4d3238a90b199b162bc2e9a5725b
Jeremy Then <32010457+jeremythen@users.noreply.github.com>
2020-08-15 22:52:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Welcome to My Movie DB");
+});
+
+app.listen(port, () => {
+    console.log(`Running at port ${port}`);
+});
+
